6ef4a63793358e7491cd30cbcfcb716f1f8c5e9d
HunterBerg <60203180+HunterBerg@users.noreply.github.com>
2021-05-06 18:38:03 -0600

    added ping command

--- main.py
+++ main.py
@@ -1,4 +1,41 @@
 import discord
 from discord.ext import commands
+Bot = commands.Bot(command_prefix = '*')
 
-#comment 
+@Bot.command()
+ 
+async def ping(ctx):
+     await ctx.send("Pong!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Bot.run("ODMzMDYyMjA3Mjg5MTYzNzc2.YHs3ow.Wqo1fu2SRAxUh2EdEnY_L6UTAlc")
