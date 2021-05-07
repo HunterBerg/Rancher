a77641b4bc91d0b001a3dd235324c2dc44b12722
HunterBerg <60203180+HunterBerg@users.noreply.github.com>
2021-05-06 18:40:14 -0600

    Revert "added ping command"
    This reverts commit 6ef4a63793358e7491cd30cbcfcb716f1f8c5e9d.

--- main.py
+++ main.py
@@ -1,41 +1,4 @@
 import discord
 from discord.ext import commands
-Bot = commands.Bot(command_prefix = '*')
 
-@Bot.command()
- 
-async def ping(ctx):
-     await ctx.send("Pong!")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-Bot.run("ODMzMDYyMjA3Mjg5MTYzNzc2.YHs3ow.Wqo1fu2SRAxUh2EdEnY_L6UTAlc")
+#comment 
