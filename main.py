a1bcb5a70f3deee1d599cfbc59478ba4dbc84a31
Hunter Berg <>
2021-05-19 02:25:38 +0000

    trying to add skip command

--- main.py
+++ main.py
@@ -93,123 +93,125 @@ class Music(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
-@commands.command()
-async def join(self, ctx,*,channel: discord.VoiceChannel):
-	if not ctx.message.author.voice:
-		await ctx.send("You are not connected to a voice channel!")
-		return
-	else:
-		channel = ctx.message.author.voice.channel
-		await ctx.send(f'Connected to ``{channel}``')
+	@commands.command()
+	async def join(self, ctx,*,channel: discord.VoiceChannel):
+		if not ctx.message.author.voice:
+			await ctx.send("You are not connected to a voice channel!")
+			return
+		else:
+			channel = ctx.message.author.voice.channel
+			await ctx.send(f'Connected to ``{channel}``')
 
-	await channel.connect()
+		await channel.connect()
 
-@commands.command()
-async def play(self, ctx, *, url):
-	try:
-		async with ctx.typing():
-			player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
-			ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
-
-		await ctx.send(f':mag_right: **Searching for** ``' + url + '``\n<:youtube:763374159567781890> **Now Playing:** ``{}'.format(player.title) + "``")
-
-	except:
-		await ctx.send("Somenthing went wrong - please try again later!")
-
-@commands.command()
-async def play_queue(self, ctx):
-	for url in queue:
+	@commands.command()
+	async def play(self, ctx, *, url):
 		try:
 			async with ctx.typing():
 				player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
 				ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
 
-			await ctx.send(f'<:youtube:763374159567781890> **Now Playing:** ``{url}``')
+			await ctx.send(f':mag_right: **Searching for** ``' + url + '``\n<:youtube:763374159567781890> **Now Playing:** ``{}'.format(player.title) + "``")
 
 		except:
 			await ctx.send("Somenthing went wrong - please try again later!")
-	
-	else:
-		await ctx.send("Queue is now done!")
 
-@commands.command()
-async def pause(self, ctx):
-	voice = get(self.bot.voice_clients, guild=ctx.guild)
-	voice.pause()
+	@commands.command()
+	async def play_queue(self, ctx):
+		for url in queue:
+			try:
+				async with ctx.typing():
+					player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
+					ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
 
-	user = ctx.message.author.mention
-	await ctx.send(f"Bot was paused by {user}")
+				await ctx.send(f'**Now Playing:** ``{url}``')
 
-@commands.command()
-async def resume(self, ctx):
-	voice = get(self.bot.voice_clients, guild=ctx.guild)
+			except:
+				await ctx.send("Somenthing went wrong - please try again later!")
+		
+		else:
+			await ctx.send("Queue is now done!")
 
-	voice.resume()
+	@commands.command()
+	async def pause(self, ctx):
+		voice = get(self.bot.voice_clients, guild=ctx.guild)
+		voice.pause()
 
-	user = ctx.message.author.mention
-	await ctx.send(f"Bot was resumed by {user}")
-
-@commands.command()
-async def add_queue(self, ctx, url):
-	global queue
-
-	try:
-		queue.append(url)
 		user = ctx.message.author.mention
-		await ctx.send(f'``{url}`` was added to the queue by {user}!')
-	except:
-		await ctx.send(f"Couldnt add {url} to the queue!")
+		await ctx.send(f"Bot was paused by {user}")
 
-@commands.command()
-async def remove_queue(self, ctx, number):
-	global queue
+	@commands.command()
+	async def resume(self, ctx):
+		voice = get(self.bot.voice_clients, guild=ctx.guild)
 
-	try:
-		del(queue[int(number)])
+		voice.resume()
+
+		user = ctx.message.author.mention
+		await ctx.send(f"Bot was resumed by {user}")
+
+	@commands.command()
+	async def add_queue(self, ctx, *, url):
+		global queue
+
+		try:
+			queue.append(url)
+			user = ctx.message.author.mention
+			await ctx.send(f'``{url}`` was added to the queue by {user}!')
+		except:
+			await ctx.send(f"Couldnt add {url} to the queue!")
+
+	@commands.command()
+	async def remove_queue(self, ctx, number):
+		global queue
+
+		try:
+			del(queue[int(number)])
+			if len(queue) < 1:
+				await ctx.send("Your queue is empty now!")
+			else:
+				await ctx.send(f'Your queue is now {queue}')
+		except:
+			await ctx.send("List index out of range - the queue starts at 0")
+
+	@commands.command()
+	async def clear_queue(self, ctx):
+
+		global queue
+
+		queue.clear()
+		user = ctx.message.author.mention
+		await ctx.send(f"The queue was cleared by {user}")
+
+	@commands.command()
+	async def view_queue(self, ctx):
+
 		if len(queue) < 1:
-			await ctx.send("Your queue is empty now!")
+			await ctx.send("The queue is empty - nothing to see here!")
 		else:
 			await ctx.send(f'Your queue is now {queue}')
-	except:
-		await ctx.send("List index out of range - the queue starts at 0")
 
-@commands.command()
-async def clear_queue(self, ctx):
+	@commands.command()
+	async def leave(self, ctx):
+		voice_client = ctx.message.guild.voice_client
+		user = ctx.message.author.mention
+		await voice_client.disconnect()
+		await ctx.send(f'Disconnected from {user}')
 
-	global queue
+	@play_queue.before_invoke
+	@play.before_invoke
+	async def ensure_voice(self, ctx):
+		if ctx.voice_client is None:
+			if ctx.author.voice:
+				await ctx.author.voice.channel.connect()
+			else:
+				await ctx.send("You are not connected to a voice channel.")
+		elif ctx.voice_client.is_playing():
+				ctx.voice_client.stop()
 
-	queue.clear()
-	user = ctx.message.author.mention
-	await ctx.send(f"The queue was cleared by {user}")
+@command.commands():
 
-@commands.command()
-async def view_queue(self, ctx):
 
-	if len(queue) < 1:
-		await ctx.send("The queue is empty - nothing to see here!")
-	else:
-		await ctx.send(f'Your queue is now {queue}')
-
-@commands.command()
-async def leave(self, ctx):
-	voice_client = ctx.message.guild.voice_client
-	user = ctx.message.author.mention
-	await voice_client.disconnect()
-	await ctx.send(f'Disconnected from {user}')
-
-@play_queue.before_invoke
-@play.before_invoke
-async def ensure_voice(self, ctx):
-	if ctx.voice_client is None:
-		if ctx.author.voice:
-			await ctx.author.voice.channel.connect()
-		else:
-			await ctx.send("You are not connected to a voice channel.")
-	elif ctx.voice_client.is_playing():
-			ctx.voice_client.stop()
-
-def setup(client):
-	client.add_cog(Music(client))
+Bot.add_cog(Music(Bot))
 
 
 
